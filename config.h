3797b786d4595ad9a520803359e3fedcb28d82c0
Lance Olsen <olsen.lance@gmail.com>
2024-12-30 15:43:36 -0800

    Modified keymap

--- config.h
+++ config.h
@@ -1,6 +1,3 @@
-// Copyright 2022 grooovebob (@grooovebob)
-// SPDX-License-Identifier: GPL-2.0-or-later
-
 #pragma once
 
 #define MATRIX_ROWS 8
@@ -20,8 +17,7 @@
     { GP8, GP9, GP16, NO_PIN, NO_PIN } \
 }
 
-
-/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+/* Debounce reduces chatter (unintended double-presses) */
 #define DEBOUNCE 5
 
 /* Serial Config */
@@ -29,41 +25,12 @@
 #define SERIAL_USART_TX_PIN GP1
 #define MASTER_LEFT
 
-/* I2C Pin mapping */
-//#define I2C1_SDA_PIN GP7
-//#define I2C1_SCL_PIN GP6
-//#define I2C1_CLOCK_SPEED 400000
-//#define I2C_DRIVER I2CD2
-
-// Set the mouse settings to a comfortable speed/accuracy trade-off,
-// assuming a screen refresh rate of 60 Htz or higher
-// The default is 50. This makes the mouse ~3 times faster and more accurate
-#define MOUSEKEY_INTERVAL 16
-// The default is 20. Since we made the mouse about 3 times faster with the previous setting,
-// give it more time to accelerate to max speed to retain precise control over short distances.
-#define MOUSEKEY_TIME_TO_MAX 40
-// The default is 300. Let's try and make this as low as possible while keeping the cursor responsive
-#define MOUSEKEY_DELAY 100
-// It makes sense to use the same delay for the mouseweel
-#define MOUSEKEY_WHEEL_DELAY 100
-// The default is 100
-#define MOUSEKEY_WHEEL_INTERVAL 50
-// The default is 40
-#define MOUSEKEY_WHEEL_TIME_TO_MAX 100
-
-// Pick good defaults for enabling homerow modifiers
+/* Tapping terms */
 #define TAPPING_TERM 200
-#define PERMISSIVE_HOLD
-#define USB_VBUS_PIN 19
-#define AUDIO_PWM_DRIVER PWMD2
-#define AUDIO_PWM_CHANNEL RP2040_PWM_CHANNEL_B
-#define AUDIO_STATE_TIMER GPTD4
-#define AUDIO_PIN GP5
-#ifdef AUDIO_ENABLE
-    #define AUDIO_INIT_DELAY
-    #define AUDIO_CLICKY
-    #define AUDIO_CLICKY_FREQ_RANDOMNESS 1.0f
-    #define STARTUP_SONG SONG(STARTUP_SOUND)
-#endif
+#define QUICK_TAP_TERM 150
 
-#define STARTUP_SOUND E__NOTE(_E6), E__NOTE(_A6), ED_NOTE(_E7),
+/* One shot modifiers */
+#define ONESHOT_TAP_TOGGLE 2
+#define ONESHOT_TIMEOUT 5000
+
+#define USB_VBUS_PIN 19
--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -1,114 +1,237 @@
 #include QMK_KEYBOARD_H
+#include "timer.h"
 
 enum dasbob_layers {
   _QWERTY,
-  _LOWER,
-  _RAISE
+  _NUMBER,
+  _SYMBOL,
+  _NAV,
 };
 
+typedef struct {
+    uint16_t tap_key;
+    uint16_t hold_mod;
+} tap_dance_data_t;
+
+void td_mod_finished(tap_dance_state_t *state, void *user_data) {
+    tap_dance_data_t *data = (tap_dance_data_t *)user_data;
+    if (state->pressed) {
+        register_code(data->hold_mod);
+    } else if (state->count == 1) {
+        tap_code16(data->tap_key);
+    }
+}
+
+void td_mod_reset(tap_dance_state_t *state, void *user_data) {
+    tap_dance_data_t *data = (tap_dance_data_t *)user_data;
+    unregister_code(data->hold_mod);
+}
+
+enum {
+  TD_SFT_PCT = 0,
+  TD_CTL_DOL = 1,
+  TD_ALT_CRT = 2,
+  TD_GUI_PCT = 3,
+  TD_SFT_LPA = 4,
+  TD_CTL_RPA = 5,
+  TD_CTL_CLN = 6,
+};
+
+tap_dance_data_t td_shift_percent = { S(KC_5), KC_LSFT };
+tap_dance_data_t td_ctrl_dollar = { S(KC_4), KC_LCTL };
+tap_dance_data_t td_alt_caret = { S(KC_6), KC_LALT };
+tap_dance_data_t td_gui_percent = { S(KC_5), KC_LGUI };
+tap_dance_data_t td_shift_lparen = { S(KC_9), KC_LSFT };
+tap_dance_data_t td_ctrl_rparen = { S(KC_0), KC_LCTL };
+tap_dance_data_t td_ctrl_colon = { S(KC_SCLN), KC_LCTL };
+
+#define ACTION_TAP_DANCE_FN_ADVANCED_USER(user_fn_on_each_tap, user_fn_on_dance_finished, user_fn_on_dance_reset, user_user_data) \
+        { .fn = {user_fn_on_each_tap, user_fn_on_dance_finished, user_fn_on_dance_reset}, .user_data = (void*)user_user_data, }
+
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_SFT_PCT] = ACTION_TAP_DANCE_FN_ADVANCED_USER(NULL, td_mod_finished, td_mod_reset, &td_shift_percent),
+    [TD_CTL_DOL] = ACTION_TAP_DANCE_FN_ADVANCED_USER(NULL, td_mod_finished, td_mod_reset, &td_ctrl_dollar),
+    [TD_ALT_CRT] = ACTION_TAP_DANCE_FN_ADVANCED_USER(NULL, td_mod_finished, td_mod_reset, &td_alt_caret),
+    [TD_GUI_PCT] = ACTION_TAP_DANCE_FN_ADVANCED_USER(NULL, td_mod_finished, td_mod_reset, &td_gui_percent),
+    [TD_SFT_LPA] = ACTION_TAP_DANCE_FN_ADVANCED_USER(NULL, td_mod_finished, td_mod_reset, &td_shift_lparen),
+    [TD_CTL_RPA] = ACTION_TAP_DANCE_FN_ADVANCED_USER(NULL, td_mod_finished, td_mod_reset, &td_ctrl_rparen),
+    [TD_CTL_CLN] = ACTION_TAP_DANCE_FN_ADVANCED_USER(NULL, td_mod_finished, td_mod_reset, &td_ctrl_colon),
+};
+
+enum custom_keycodes {
+    KC_REV_SEMI = SAFE_RANGE,
+};
+
+// Set to true any time KC_REV_SEMI is being pressed
+bool is_gui_down = false;
+
+/* Set to true after KC_REV_SEMI has been held for TAPPING_TERM, or if another key is pressed and released while
+ * KC_REV_SEMI is down (resulting in sending the GUI-modified version) */
+bool is_gui_held = false;
+
+// Set to true when deferring decision-making on whether to send the GUI-modified or regular version of a key
+// Right now, only a single deferred keycode is supported
+bool is_keycode_deferred = false;
+uint16_t gui_timer;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case KC_REV_SEMI:
+            // Send : when tapped (held less than TAPPING_TERM)
+            // Send ; when tapped (held less than TAPPING_TERM) with Shift or OSM Shift
+            // If another key is pressed and released while KC_REV_SEMI is down, send the GUI-modified version, even
+            // if the KC_REV_SEMI key was not held more than TAPPING_TERM
+
+            if (record->event.pressed) {
+                // Key pressed
+                gui_timer = timer_read();
+                is_gui_down = true;
+                is_gui_held = false;
+            } else {
+                // Key released
+                is_gui_down = false;
+
+                if (is_gui_held) {
+                    // Do nothing if the key was held longer than TAPPING_TERM, or was used as a modifier because
+                    // another key was pressed and released while KC_REV_SEMI was down
+                    unregister_mods(MOD_BIT(KC_LGUI));
+                    is_gui_held = false;
+                } else {
+                    if (get_mods() & MOD_BIT(KC_LSFT)) {
+                        // If Shift is held, send ;
+                        unregister_code(KC_LSFT);
+                        tap_code(KC_SCLN);
+                        register_code(KC_LSFT);
+                    } else if (get_oneshot_mods() & MOD_MASK_SHIFT) {
+                        // If OSM Shift was tapped, send ;
+                        del_oneshot_mods(MOD_MASK_SHIFT);
+                        tap_code(KC_SCLN);
+                    } else {
+                        // If no Shift behavior is activated, send :
+                        tap_code16(S(KC_SCLN));
+                    }
+                }
+            }
+            return false; // Skip further processing for this key
+        default:
+            if (is_gui_down) {
+                if (record->event.pressed) {
+                    // Wait on deciding what to send until after one of the keys is released
+                    is_keycode_deferred = true;
+                    return false;
+                } else if (is_keycode_deferred) {
+                    // If a key is pressed and released before the KC_REV_SEMI key is released, send the mod version
+                    register_mods(MOD_BIT(KC_LGUI));
+                    register_code(keycode);
+                    unregister_code(keycode);
+                    is_keycode_deferred = false;
+                    is_gui_held = true; // Prevent sending : or ; even if KC_REV_SEMI is released before TAPPING_TERM
+                    return false;
+                }
+            } else {
+                // If a key was deferred but KC_REV_SEMI was released first, send the normal keycode
+                if (!record->event.pressed && is_keycode_deferred) {
+                    is_keycode_deferred = false;
+                    register_code(keycode);
+                    unregister_code(keycode);
+                }
+            }
+    }
+    return true; // Process all other keycodes as usual
+}
+
+void matrix_scan_user(void) {
+    // Set is_gui_held if the KC_REV_SEMI key is held longer than TAPPING_TERM
+    if (is_gui_down && !is_gui_held && timer_elapsed(gui_timer) > TAPPING_TERM) {
+        register_mods(MOD_BIT(KC_LGUI));
+        is_gui_held = true;
+    }
+}
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-     /*
+     /* QWERTY (0)
       * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
       * │ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │
       * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
-      * │ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ ; │
+      * │A g│S o│D c│F s│G h│       │H h│J s│K c│L o│:;g│
       * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
       * │ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ / │
       * └───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┘
-      *           ┌───┐                   ┌───┐
-      *           │DEL├───┐           ┌───┤ESC│
-      *           └───┤SPC├───┐   ┌───┤BSP├───┘
-      *               └───│LOW│   │RSE├───┘
-      *                   └───┘   └───┘
+      *     ┌─────┐                           ┌─────┐
+      *     │1,BSP├─────┐               ┌─────┤ ESC │
+      *     └─────┤2,SPC├─────┐   ┌─────┤2,ENT├─────┘
+      *           └─────│3,TAB│   │ SFT ├─────┘
+      *                 └─────┘   └─────┘
+      * NUMBER (1)
+      * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
+      * │   │   │   │   │ T │       │ * │ 7 │ 8 │ 9 │ + │
+      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
+      * │ g │ o │: c│% s│   │       │ / │ 4 │ 5 │ 6 │ - │
+      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
+      * │   │   │   │   │   │       │ 0 │ 1 │ 2 │ 3 │ . │
+      * └───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┘
+      *     ┌─────┐                           ┌─────┐
+      *     │     ├─────┐               ┌─────┤ BSP │
+      *     └─────┤     ├─────┐   ┌─────┤     ├─────┘
+      *           └─────│     │   │     ├─────┘
+      *                 └─────┘   └─────┘
+      * SYMBOL (2)
+      * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
+      * │ * │ @ │ # │ ~ │ + │       │   │ { │ } │   │   │
+      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
+      * │% g│^ o│$ c│- s│ = │       │ " │( s│) c│' o│` g│
+      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
+      * │ ! │ & │ | │ _ │ \ │       │   │ [ │ ] │   │   │
+      * └───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┘
+      *     ┌─────┐                           ┌─────┐
+      *     │ BSP ├─────┐               ┌─────┤ BSP │
+      *     └─────┤     ├─────┐   ┌─────┤     ├─────┘
+      *           └─────│     │   │     ├─────┘
+      *                 └─────┘   └─────┘
+      * NAV (3)
+      * ↖ ↑ ↗
+      * ← · →
+      * ↙ ↓ ↘
+      * ┌───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┐
+      * │   │   │   │   │   │       │swu│prv│ply│nxt│   │
+      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
+      * │ g │ o │ c │ s │   │       │ ← │ ↓ │ ↑ │ → │   │
+      * ├───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┤
+      * │   │   │   │   │   │       │swd│mut│ v+│ v-│   │
+      * └───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┘
+      *     ┌─────┐                           ┌─────┐
+      *     │     ├─────┐               ┌─────┤     │
+      *     └─────┤     ├─────┐   ┌─────┤     ├─────┘
+      *           └─────│     │   │     ├─────┘
+      *                 └─────┘   └─────┘
       */
 
 
     [_QWERTY] = LAYOUT_split_3x5_3(
-        KC_Q,    KC_W,              KC_E,            KC_R,            KC_T,               KC_Y,         KC_U,              KC_I,              KC_O,           KC_P,
-        KC_A,    RALT_T(KC_S),      LCTL_T(KC_D),    LSFT_T(KC_F),    KC_G,               KC_H,         LSFT_T(KC_J),      LCTL_T(KC_K),      RALT_T(KC_L),   KC_SCLN,
-        KC_Z,    KC_X,              KC_C,            KC_V,            KC_B,               KC_N,         KC_M,              KC_COMM,           KC_DOT,         KC_SLSH,
-                                    KC_DEL,          KC_SPC,          MO(1),              MO(2),        KC_BSPC,           KC_ESC
+        KC_Q,            KC_W,            KC_E,            KC_R,            KC_T,                 KC_Y,            KC_U,            KC_I,            KC_O,            KC_P,
+        LGUI_T(KC_A),    LALT_T(KC_S),    LCTL_T(KC_D),    LSFT_T(KC_F),    MT(MOD_HYPR, KC_G),   MT(MOD_HYPR, KC_H), LSFT_T(KC_J), RCTL_T(KC_K),    LALT_T(KC_L),    KC_REV_SEMI, // RGUI_T(KC_REV_SEMI),
+        KC_Z,            KC_X,            KC_C,            KC_V,            KC_B,                 KC_N,            KC_M,            KC_COMM,         KC_DOT,          KC_SLSH,
+                                          LT(1,KC_BSPC),   LT(2,KC_SPC),    LT(3,KC_TAB),         OSM(MOD_LSFT),   LT(2,KC_ENT),    KC_ESC
     ),
+    [_NUMBER] = LAYOUT_split_3x5_3(
+        XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         KC_T,                 KC_PAST,         KC_7,            KC_8,            KC_9,            KC_PPLS,
+        KC_LGUI,         KC_LALT,         TD(TD_CTL_CLN),  TD(TD_SFT_PCT),  XXXXXXX,              KC_PSLS,         KC_4,            KC_5,            KC_6,            KC_PMNS,
+        XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,              KC_0,            KC_1,            KC_2,            KC_3,            KC_PDOT,
 
-    [_LOWER] = LAYOUT_split_3x5_3(
-       KC_1,     KC_2,              KC_3,            KC_4,            KC_5,               KC_6,         KC_7,             KC_8,            KC_9,              KC_0,
-       CK_RST,  RALT_T(XXXXXXX),   LCTL_T(XXXXXXX), LSFT_T(XXXXXXX), KC_PSCR,           KC_MINS,      LSFT_T(KC_EQL),   LCTL_T(KC_GRV),  RALT_T(KC_QUOT),    KC_BSLS,
-       CK_TOGG,  CK_UP,           CK_DOWN,         LCTL(KC_C),      LCTL(KC_V),         KC_TAB,      XXXXXXX,          XXXXXXX,         XXXXXXX,           XXXXXXX,
-                                    KC_TRNS,         KC_TRNS,         KC_TAB,             KC_ENT,       KC_TRNS,          KC_TRNS
+                                          XXXXXXX,         XXXXXXX,         XXXXXXX,              XXXXXXX,         XXXXXXX,         KC_BSPC
     ),
+    [_SYMBOL] = LAYOUT_split_3x5_3(
+        S(KC_8),         S(KC_2),         S(KC_3),         S(KC_GRAVE),     S(KC_EQL),            MS_WHLU,         S(KC_LBRC),      S(KC_RBRC),      XXXXXXX,         XXXXXXX,
+        TD(TD_GUI_PCT),  TD(TD_ALT_CRT),  TD(TD_CTL_DOL),  LSFT_T(KC_MINS), KC_EQUAL,             S(KC_QUOT),      TD(TD_SFT_LPA),  TD(TD_CTL_RPA),  LALT_T(KC_QUOT), RGUI_T(KC_GRAVE),
+        S(KC_1),         S(KC_7),         S(KC_BSLS),      S(KC_MINS),      KC_BSLS,              MS_WHLD,         KC_LBRC,         KC_RBRC,         XXXXXXX,         S(KC_SLSH),
 
-    [_RAISE] = LAYOUT_split_3x5_3(
-        KC_ESC,   KC_UP,           XXXXXXX,         XXXXXXX,         CK_TOGG,             KC_NUM_LOCK,  KC_KP_7,          KC_KP_8,         KC_KP_9,           KC_KP_MINUS,
-        KC_LEFT,  RALT_T(KC_DOWN), LCTL_T(KC_RGHT), LSFT_T(XXXXXXX), KC_LGUI,             XXXXXXX,      LSFT_T(KC_KP_4),  LCTL_T(KC_KP_5), RALT_T(KC_KP_6),   KC_KP_PLUS,
-        KC_MPRV,  KC_MSTP,         KC_MPLY,         KC_MNXT,         KC_PSCR,             XXXXXXX,      KC_KP_1,          KC_KP_2,         KC_KP_3,           KC_KP_0,
-                                   KC_TRNS,         KC_TRNS,         KC_TAB,              KC_ENT,       KC_TRNS,          KC_TRNS
+                                          KC_BSPC,         XXXXXXX,         XXXXXXX,              XXXXXXX,         XXXXXXX,         KC_BSPC
     ),
+    [_NAV] = LAYOUT_split_3x5_3(
+        XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,              XXXXXXX,         KC_MPRV,         KC_MPLY,           KC_MNXT,         XXXXXXX,
+        KC_LGUI,         KC_LALT,         KC_LCTL,         KC_LSFT,         XXXXXXX,              KC_LEFT,         KC_DOWN,         KC_UP,             KC_RIGHT,        XXXXXXX,
+        XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,         XXXXXXX,              XXXXXXX,         KC_KB_MUTE,      KC_KB_VOLUME_DOWN, KC_KB_VOLUME_UP, XXXXXXX,
 
-
+                                          XXXXXXX,         XXXXXXX,         XXXXXXX,              XXXXXXX,         XXXXXXX,         XXXXXXX
+    )
 };
-#ifdef OLED_ENABLE
-
-oled_rotation_t oled_init_user(oled_rotation_t rotation) { return OLED_ROTATION_0; }
-bool oled_task_user(void) {
-    if (is_keyboard_master()) {
-        static const char PROGMEM runqmk_logo[] = {0,0,0,0,152,152,152,152,152,152,24,24,24,
-        152,152,24,24,24,152,152,24,24,152,152,24,24,24,152,152,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,127,127,13,13,29,63,103,64,0,63,127,96,96,96,127,63,0,0,127,127,7,12,56,
-        127,127,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,120,252,134,198,198,252,120,0,0,254,254,
-        60,224,224,60,254,254,0,254,254,16,24,124,230,130,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,24,24,25,25,25,25,27,24,24,25,25,24,25,25,24,25,25,24,25,25,24,24,24,24,25,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,0,0,0};
-
-
-        oled_write_raw_P(runqmk_logo, sizeof(runqmk_logo));
-
-        led_t led_usb_state = host_keyboard_led_state();
-        oled_set_cursor(6, 3);
-        oled_write_P(led_usb_state.num_lock    ? PSTR("NUM") : PSTR(""), false);
-        oled_set_cursor(6, 2);
-        oled_write_P(PSTR("WPM: "), false);
-        oled_write(get_u8_str(get_current_wpm(), '0'), false);
-        oled_set_cursor(6, 0);
-        oled_write_P(PSTR("by GroooveBob"), false);
-            oled_set_cursor(6, 1);
-            oled_write_P(PSTR("Layer: "), false);
-
-    switch (get_highest_layer(layer_state)) {
-        case _QWERTY:
-        oled_set_cursor(12, 1);
-            oled_write_P(PSTR("Default\n"), false);
-            break;
-        case _LOWER:
-        oled_set_cursor(12, 1);
-            oled_write_P(PSTR("Lower\n"), false);
-            break;
-        case _RAISE:
-        oled_set_cursor(12, 1);
-            oled_write_P(PSTR("Raise\n"), false);
-            break;
-        default:
-            // Or use the write_ln shortcut over adding '\n' to the end of your string
-        oled_set_cursor(6, 1);
-        oled_write_ln_P(PSTR("Undefined"), false);
-    }
-    } else {
-        static const char PROGMEM dasbob_logo[] = {
-        0,  0,  0,  0,  0,128,128,128,128,128,128,128,128,128,128,128,128,128,128,128,  0,  0,  0,  0,  0,128,128,128,192,192,192,192,192,192,192,192,192,192,192,128,128,  0,  0,  0,  0,  0,  0,  0,128,128,128,128,128,128,128,128,128,128,128,128,128,128,128,  0,  0,  0,  0,  0,128,128,128,128,128,128,128,128,128,128,128,128,128,  0,  0,  0,128,128,128,128,128,192,192,192,192,192,  0,  0,  0,128,128,128,128,128,128,128,128,128,  0,  0,  0,  0,128,128,128,128,128,128,128,128,128,128,128,128,128,128,128,128,  0,  0,
-        0,  0,126,127,255,255,255,255,255,255,255,199,247,255,255,255,255,255,255,255,255,127,252,255,255,255,255,255,255,255,255,255, 15,127,255,255,255,255,255,255,255,255,254,248,  0,126,127,255,255,255,255,255,255,255,199,247,255,255,255,255,255,255,255,255,127,126,255,255,255,255,255,255,255,255,239,239,239,239,207,223,223,223,159, 63,  0,  7, 63,255,255,255,255,255,255,255,254, 14,254,255,255,255,255,255,255,255, 63,241,252,254,255,255,255,255,255,255,255, 31,255,255,255,255,255,255,255,255,255,255,128,  0,
-        0,  0,126,255,255,255,255,255,255,255,255,227,255,255,255,255,255,255,255,255,255,  0, 15, 63,127,255,255,255,255,255,255,255,248,255,255,255,255,255,255,255,255,127, 63,  7,  0,126,255,255,255,255,255,255,255,255,227,255,255,255,255,255,255,255,255,255,128,252,253,249,249,251,251,251,251,251,255,255,255,255,255,255,255,255,127, 30,  0,  0,  0,  3, 31,255,255,255,255,255,255,255,255,255,255,255,255,255,255,  7,  0, 63,255,255,255,255,255,255,255,255,255,248,255,255,255,255,255,255,255,255,255,255,255,  0,
-        0,  0,  0,  0,  0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0,  0,  0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0,
-        };
-        oled_write_raw_P(dasbob_logo, sizeof(dasbob_logo));
-    }
-    return false;
-}
-#endif
